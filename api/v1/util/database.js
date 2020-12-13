1ee19652d21cc3b56c0ad41178be2804d7b696dd
pbolinao <47652863+pbolinao@users.noreply.github.com>
2020-12-13 12:50:48 -0800

    Update database.js

--- api/v1/util/database.js
+++ api/v1/util/database.js
@@ -1,17 +1,12 @@
 const mysql = require('mysql2');
 const host = "localhost";
-const user = "jamespca_CMG";
-const password = "To}u_i(0%]Fg";
 const database = "jamespca_composermon";
 
-// connect to a database jamespca_composermon running on the server locally
 const pool = mysql.createPool({
     host: host,
     port: process.env.PORT,
-    // user: user,
     user: "root",
     database: database,
-    // password: password
     password: ""
 });
 
